048ca3626669535f84ba121f20d57cc77968cd58
Dominik <kubickidom@icloud.com>
2022-06-30 21:12:54 +0200

    gwiazdka w scanf i printf

--- Rozdzial4_3/main.c
+++ Rozdzial4_3/main.c
@@ -12,9 +12,13 @@ int main(int argc, const char * argv[]) {
     printf("Hello, World!\n");
     int wiek;
     int liczba = 256;
+    int druga;
     printf("Liczba jest rowna = %*d\n", 10, liczba);
+    printf("scanf z * pomij liczbe. Podaj 2 liczby\n");
+    scanf("%*d %d", &druga);
+    printf("Druga = %d\n", druga);
     scanf("%d",&wiek);
-    printf("%d",wiek);
+    printf("%d\n",wiek);
     return 0;
     
     
